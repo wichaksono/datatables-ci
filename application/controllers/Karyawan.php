6d4ee5bce9d21bcb88f36293dcb6d1d2ece8820e
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2016-02-16 21:36:23 +0700

    Update Karyawan.php
    Pembaruan Kode untuk alternatif query join pada datatables codeigniter

--- application/controllers/Karyawan.php
+++ application/controllers/Karyawan.php
@@ -17,51 +17,34 @@ class Karyawan extends CI_Controller {
     		strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'
     		)
     	{
+            
             $this->load->model('M_Karyawan');
     		
     		/**
     		 * Mengambil Parameter dan Perubahan nilai dari setiap 
     		 * aktifitas pada table
     		 */
-    		$start  = $this->input->post('start') > 0 
-    		          ? $this->input->post('start') 
-    		          : 0;
+            $datatables  = $_POST;
+            $datatables['table']    = 'karyawan';
+    		$datatables['id-table'] = 'id_karyawan';
 
-    		$limit  = $this->input->post('length') > 0 
-    		          ? $this->input->post('length') 
-    		          : 10;
+            /**
+             * Kolom yang ditampilkan
+             */
+	    	$datatables['col-display'] = array(
+            	    		               'first_name',
+            	    		               'last_name',
+            	    		               'position_name',
+            	    		               'office',
+            	    		               'start_date'
+            	    		             );
 
-    		$column = isset( $this->input->post('order')[0]['column'] ) 
-    		          ? $this->input->post('order')[0]['column'] 
-    		          : 0;
+            /**
+             * menggunakan table join
+             */
+            $datatables['join']    = 'INNER JOIN position ON position = id_position';
 
-    		$order  = isset( $this->input->post('order')[0]['dir'] ) 
-    		          ? $this->input->post('order')[0]['dir'] 
-    		          : 'asc';
-
-    		$search = $this->input->post('search')['value'] != '' 
-    		          ? $this->input->post('search')['value'] 
-    		          : '';
-    		
-
-	    	$columns = array(
-	    		           'first_name',
-	    		           'last_name',
-	    		           'position',
-	    		           'office',
-	    		           'start_date'
-	    		        );
-
-
-    		$property = array(
-			               'start' => $start,
-			               'limit' => $limit,
-			               'order' => $order,
-			               'search'=> $search,
-			               'column_order' =>$column
-			            );
-
-	    	$this->M_Karyawan->Datatables('karyawan', $columns, $property);
+	    	$this->M_Karyawan->Datatables($datatables);
     	}
 
     	return;
