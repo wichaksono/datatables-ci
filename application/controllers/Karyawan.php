ca658c635a294a7163dbd37d19308b80b1baefa2
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2016-02-16 21:38:52 +0700

    Update Karyawan.php
    Perubahan kode pembuat Json untuk datatables codeigniter

--- application/controllers/Karyawan.php
+++ application/controllers/Karyawan.php
@@ -1,52 +1,111 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Karyawan extends CI_Controller {
+class M_Karyawan extends CI_Model
+{
     
-    public function index()
+    function __construct()
     {
-    	$this->load->view('view_karyawan');
+        parent::__construct();
     }
 
-    function datatables_ajax()
+    function Datatables($dt)
     {
-    	/** AJAX Handle */
-    	if(
-    		isset($_SERVER['HTTP_X_REQUESTED_WITH']) && 
-    		!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && 
-    		strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'
-    		)
-    	{
+        $columns = implode(', ', $dt['col-display']) . ', ' . $dt['id-table'];
+
+        $join = $dt['join'];
+
+        $sql  = "SELECT {$columns} FROM {$dt['table']} {$join}";
+
+
+        $data = $this->db->query($sql);
+
+        $rowCount = $data->num_rows();
+
+        $data->free_result();
+
+        // pengkondisian aksi seperti next, search dan limit
+        $columnd = $dt['col-display'];
+        $count_c = count($columnd);
+
+        // search
+        $search = $dt['search']['value'];
+
+        /**
+         * Search Global
+         * pencarian global pada pojok kanan atas
+         */
+        $where = '';
+        if ($search != '') {   
+            for ($i=0; $i < $count_c ; $i++) {
+                $where .= $columnd[$i] .' LIKE "%'. $search .'%"';
+                
+                if ($i < $count_c - 1) {
+                    $where .= ' OR ';
+                }
+            }
+        }
+        
+        /**
+         * Search Individual Kolom
+         * pencarian dibawah kolom
+         */
+        for ($i=0; $i < $count_c; $i++) { 
+            $searchCol = $dt['columns'][$i]['search']['value'];
+            if ($searchCol != '') {
+                $where = $columnd[$i] . ' LIKE "%' . $searchCol . '%" ';
+                break;
+            }
+        }
+
+        /**
+         * pengecekan Form pencarian
+         * pencarian aktif jika ada karakter masuk pada kolom pencarian.
+         */
+        if ($where != '') {
+            $sql .= " WHERE " . $where;
             
-            $this->load->model('M_Karyawan');
-    		
-    		/**
-    		 * Mengambil Parameter dan Perubahan nilai dari setiap 
-    		 * aktifitas pada table
-    		 */
-            $datatables  = $_POST;
-            $datatables['table']    = 'karyawan';
-    		$datatables['id-table'] = 'id_karyawan';
+        }
+        
+        // sorting
+        $sql .= " ORDER BY {$columnd[$dt['order'][0]['column']]} {$dt['order'][0]['dir']}";
+        
+        // limit
+        $start  = $dt['start'];
+        $length = $dt['length'];
 
-            /**
-             * Kolom yang ditampilkan
-             */
-	    	$datatables['col-display'] = array(
-            	    		               'first_name',
-            	    		               'last_name',
-            	    		               'position_name',
-            	    		               'office',
-            	    		               'start_date'
-            	    		             );
+        $sql .= " LIMIT {$start}, {$length}";
+        
+        $list = $this->db->query($sql);
 
-            /**
-             * menggunakan table join
-             */
-            $datatables['join']    = 'INNER JOIN position ON position = id_position';
+        /**
+         * convert to json
+         */
+        $option['draw']            = $dt['draw'];
+        $option['recordsTotal']    = $rowCount;
+        $option['recordsFiltered'] = $rowCount;
+        $option['data']            = array();
 
-	    	$this->M_Karyawan->Datatables($datatables);
-    	}
+        foreach ($list->result() as $row) {
+        /**
+         * custom gunakan
+         * $option['data'][] = array(
+         *                       $row->columnd[0],
+         *                       $row->columnd[1],
+         *                       $row->columnd[2],
+         *                       $row->columnd[3],
+         *                       $row->columnd[4],
+         *                       .....
+         *                     );
+         */
+           $rows = array();
+           for ($i=0; $i < $count_c; $i++) { 
+               $rows[] = $row->$columnd[$i];
+           }
+           $option['data'][] = $rows;
+        }
 
-    	return;
+        // eksekusi json
+        echo json_encode($option);
+
     }
 }
