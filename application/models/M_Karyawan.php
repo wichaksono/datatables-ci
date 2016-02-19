26ee676b3a2cda2ed53acb469216722185cd7729
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2016-02-19 11:20:33 +0700

    Update M_Karyawan.php

--- application/models/M_Karyawan.php
+++ application/models/M_Karyawan.php
@@ -2,132 +2,110 @@
 
 class M_Karyawan extends CI_Model
 {
-	
-	function __construct()
-	{
-		parent::__construct();
-	}
+    
+    function __construct()
+    {
+        parent::__construct();
+    }
 
-	function Datatables($table, $column, $queries = array())
-	{
-		/**
-		 * Nilai default ketika variable $queries tidak di isi
-		 * @var array
-		 */
-		$default = array(
-			         'start' => 0,
-			         'limit' => 10,
-			         'order' => 'asc',
-			         'column_order' => 0,
-			         'search'=> '',
-			         'column_search' => ''
-			       );
-        
+    function Datatables($dt)
+    {
+        $columns = implode(', ', $dt['col-display']) . ', ' . $dt['id-table'];
+
+        $join = $dt['join'];
+
+        $sql  = "SELECT {$columns} FROM {$dt['table']} {$join}";
+
+
+        $data = $this->db->query($sql);
+
+        $rowCount = $data->num_rows();
+
+        $data->free_result();
+
+        // pengkondisian aksi seperti next, search dan limit
+        $columnd = $dt['col-display'];
+        $count_c = count($columnd);
+
+        // search
+        $search = $dt['search']['value'];
+
         /**
-         * merge nilai saat variable $queries diberi nilai.
+         * Search Global
+         * pencarian global pada pojok kanan atas
          */
-        if (count($queries) > 0) {
-        	$default = array_merge($default, $queries);
+        $where = '';
+        if ($search != '') {   
+            for ($i=0; $i < $count_c ; $i++) {
+                $where .= $columnd[$i] .' LIKE "%'. $search .'%"';
+                
+                if ($i < $count_c - 1) {
+                    $where .= ' OR ';
+                }
+            }
         }
         
         /**
-         * Query Data 
+         * Search Individual Kolom
+         * pencarian dibawah kolom
          */
-        $this->db->select(implode(',', $column));
-        $this->db->from($table);
-
-        /**
-         * apabila fungsi Search digunakan.
-         */
-        if ($default['search'] != '') {
-        	for($i=0; $i < count($column); $i++) {
-        		if ($i == 0) {
-        			$this->db->like($column[$i], $default['search']);
-        		} else {
-        			$this->db->or_like($column[$i], $default['search']);
-        		}
-        	}
-        }
-        
-        /**
-         * fungsi untuk menangani pencarian individual 
-         * perkolom.
-         * @var array
-         */
-    	$columns = '';
-    	for($i=0; $i < count($column); $i++) {
-    		if (isset($this->input->post('columns')[$i]) &&
-    			$this->input->post('columns')[$i]['search']['value'] != '') 
-    		{
-    			$columns = array(
-    				          $column[$i],
-    				          $this->input->post('columns')[$i]['search']['value']
-    				        );
-    			break;
-    		}
-
-    	}
-        
-        /**
-         * jika pencarian perkolom diaktifkan dan digunakan
-         */
-    	if ($columns != '') {
-    		$this->db->like($columns[0], $columns[1]);
-    	}
-        
-        /**
-         * fungsi Sort perkolom
-         * $default['column_order'] 
-         * @return integer
-         */
-        $column_order = $column[ $default['column_order'] ];
-        
-		$this->db->order_by($column_order, $default['order']);
-
-		/**
-		 * Fungsi pembatas data yang ingin ditampilkan
-		 */
-        $this->db->limit($default['limit'], $default['start']);
-
-        $query = $this->db->get();
-        
-        /**
-         * hitung semua data yang ada
-         * @var [type]
-         */
-        $count = $this->db->get($table);
-        $recordsTotal = $count->num_rows();
-
-        /**
-         * jika fungsi search baik global maupun individ
-         * akan dilakukan penghitungan total
-         */
-        if ($default['search'] != '' || $columns != '') {
-        	$recordsTotal = $query->num_rows();
+        for ($i=0; $i < $count_c; $i++) { 
+            $searchCol = $dt['columns'][$i]['search']['value'];
+            if ($searchCol != '') {
+                $where = $columnd[$i] . ' LIKE "%' . $searchCol . '%" ';
+                break;
+            }
         }
 
         /**
-         * Menyusun data
+         * pengecekan Form pencarian
+         * pencarian aktif jika ada karakter masuk pada kolom pencarian.
          */
-        $options['draw']    = 0;
-		$options['recordsTotal']    = $recordsTotal;
-		$options['recordsFiltered'] = $recordsTotal;
-		$options['data'] = array();
- 
-        foreach($query->result() as $row) {
-        	$options['data'][] = array(
-        		               $row->first_name,
-        		               $row->last_name,
-        		               $row->position,
-        		               $row->office,
-        		               $row->start_date
-        		               );
+        if ($where != '') {
+            $sql .= " WHERE " . $where;
+            
         }
+        
+        // sorting
+        $sql .= " ORDER BY {$columnd[$dt['order'][0]['column']]} {$dt['order'][0]['dir']}";
+        
+        // limit
+        $start  = $dt['start'];
+        $length = $dt['length'];
+
+        $sql .= " LIMIT {$start}, {$length}";
+        
+        $list = $this->db->query($sql);
 
         /**
          * convert to json
          */
-		echo json_encode($options);
+        $option['draw']            = $dt['draw'];
+        $option['recordsTotal']    = $rowCount;
+        $option['recordsFiltered'] = $rowCount;
+        $option['data']            = array();
 
-	}
+        foreach ($list->result() as $row) {
+        /**
+         * custom gunakan
+         * $option['data'][] = array(
+         *                       $row->columnd[0],
+         *                       $row->columnd[1],
+         *                       $row->columnd[2],
+         *                       $row->columnd[3],
+         *                       $row->columnd[4],
+         *                       .....
+         *                     );
+         */
+           $rows = array();
+           for ($i=0; $i < $count_c; $i++) { 
+               $rows[] = $row->$columnd[$i];
+           }
+           $option['data'][] = $rows;
+        }
+
+        // eksekusi json
+        echo json_encode($option);
+
+    }
 }
